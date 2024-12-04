9e4ade3d1f022036d88304b0e0face08a1f7c072
hyeyeon lee <72775512+hyeyeon919@users.noreply.github.com>
2024-12-04 13:55:51 +0900

    Update test_fp16.py

--- test_fp16.py
+++ test_fp16.py
@@ -13,11 +13,3 @@ print("Exporting YOLOv8 TensorRT FP32 model...")
 model_fp32 = YOLO("yolov8n.pt")
 model_fp32.export(format="engine", dynamic=False, batch=8, workspace=4, half=True, data=yaml_path)
 
-print("Loading TensorRT FP16 model...")
-tensorrt_fp16 = YOLO("yolov8n.engine")
-
-print("Calculating FP16 model metrics on COCO128...")
-dataset_accuracy_fp16, dataset_speed_fp16 = calculate_model_metrics(tensorrt_fp16, dataset_path)
-save_results_to_file(results_file, "TensorRT FP16 (COCO128)", dataset_speed_fp16, dataset_accuracy_fp16)
-
-print("Comparison complete. Results saved to", results_file)
